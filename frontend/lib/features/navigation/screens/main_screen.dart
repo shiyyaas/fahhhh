9edec5d38fc231664e0e778f5ea18874af227cad
Shiyas ps <shiyasofficial1@gmail.com>
2026-06-08 22:04:12 +0530

    slowly converting

--- frontend/lib/features/navigation/screens/main_screen.dart
+++ frontend/lib/features/navigation/screens/main_screen.dart
@@ -11,77 +11,56 @@ import 'package:fahhhh/features/navigation/widgets/navbar.dart';
 // Models
 import 'package:fahhhh/features/navigation/models/nav_item.dart';
 
+// Riverpod
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/navigation_provider.dart';
+
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatefulWidget {
-
+class MainScreen extends ConsumerWidget {
   const MainScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedIndex = ref.watch(navigationIndexProvider);
 
-}
+    final List<NavItem> items = [
+      NavItem(icon: Icons.home_outlined, label: 'Home', page: const Home()),
 
-class _MainScreenState extends State<MainScreen> {
-
-  int selectedIndex = 0;
-
-  final List<NavItem> items = [
-
-    NavItem(
-      icon: Icons.home_outlined,
-      label: 'Home',
-      page: const Home(),
-    ),
-
-    NavItem(
-      icon: Icons.apartment_outlined,
-      label: 'Department',
-      page: const Department(),
-    ),
-
-    NavItem(
-      icon: Icons.groups_outlined,
-      label: 'Class',
-      page: const MyClass(),
-    ),
-
-    NavItem(
-      icon: Icons.menu_book_outlined,
-      label: 'Subjects',
-      page: const MySubject(),
-    ),
-
-    NavItem(
-      icon: Icons.person_outline,
-      label: 'Profile',
-      page: const Profile(),
-    ),
-
-  ];
-
-  void onItemTapped(int index) {
-
-    setState(() {
-      selectedIndex = index;
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      body: items[selectedIndex].page,
-      bottomNavigationBar: Navbar(
-        selectedIndex: selectedIndex,
-        onItemTapped: onItemTapped,
-        items: items,
+      NavItem(
+        icon: Icons.apartment_outlined,
+        label: 'Department',
+        page: const Department(),
       ),
 
-    );
+      NavItem(
+        icon: Icons.groups_outlined,
+        label: 'Class',
+        page: const MyClass(),
+      ),
 
+      NavItem(
+        icon: Icons.menu_book_outlined,
+        label: 'Subjects',
+        page: const MySubject(),
+      ),
+
+      NavItem(
+        icon: Icons.person_outline,
+        label: 'Profile',
+        page: const Profile(),
+      ),
+    ];
+
+      return Scaffold(
+        body: items[selectedIndex].page,
+        bottomNavigationBar: Navbar(
+          selectedIndex: selectedIndex,
+          onItemTapped: (index) {
+            ref.read(navigationIndexProvider.notifier).state = index;
+          },
+          items: items,
+        ),
+      );
   }
-
 }
