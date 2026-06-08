8110a67470e25db6af55ec1a2fda3c4167e7148a
Shiyas ps <shiyasofficial1@gmail.com>
2026-06-08 21:52:55 +0530

    Converting to riverpod

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:fahhhh/core/routes/app_routes.dart';
 import 'package:fahhhh/core/theme_data/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
